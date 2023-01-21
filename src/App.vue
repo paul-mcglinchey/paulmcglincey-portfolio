4d7c1195bb0e7b57b1c32e49ab04e3fa17acbf95
paul-mcglinchey <paulmcglinchey789@gmail.com>
2023-01-21 00:55:07 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HomeTitle from './components/HomeTitle.vue'
+import './index.css';
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      <HomeTitle />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
---
+++ src/components/HomeTitle.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1 class="group text-6xl font-bold tracking-wider font-mono text-blue-300 p-4">
+    <a href="mailto:paulmcglinchey789@gmail.com">
+      <span class="group">paulmcglinchey</span><span class="opacity-0 group group-hover:opacity-100 transition-all duration-300">789@gmail.com</span>
+    </a>
+  </h1>
+</template>
