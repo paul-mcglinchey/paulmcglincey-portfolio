7a4cdf1254dcfb3836e51d3e3fe9de66e4937d73
paul-mcglinchey <paulmcglinchey789@gmail.com>
2023-03-01 00:18:01 +0000

    Lots of cleanup done and some experimentation with the github API and css transitions

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,48 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 import HomeTitle from './components/HomeTitle.vue'
 import './index.css';
+import GithubWhiteIcon from './assets/icons/GithubWhiteIcon.vue'
+import { onMounted, ref } from 'vue';
+
+const githubUserData = ref()
+
+const getGitHubUserData = async () => {
+  var res = await fetch('https://api.github.com/users/paul-mcglinchey', {
+    method: 'GET'
+  })
+
+  var json = await res.json()
+
+  githubUserData.value = json
+}
+
+onMounted(() => {
+  getGitHubUserData()
+})
+
 </script>
 
 <template>
   <header>
-
-    <div class="wrapper">
+    <div class="flex justify-between">
       <HomeTitle />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
+      <div class="flex p-4">
+        <div class="relative group flex flex-grow">
+          <div class="flex flex-grow items-center justify-between relative z-20 p-5">
+            <a :href="githubUserData?.html_url" class="inline-flex items-center tracking-wide font-semibold text-gray-200 text-lg invisible opacity-0 group-hover:visible group-hover:opacity-100 transition-all hover:text-blue-300">
+              {{ githubUserData?.login }}
+              <GithubWhiteIcon class="w-5 h-5 ml-2" />
+            </a>
+            <img :src="githubUserData?.avatar_url" class="w-12 h-12 rounded-full outline outline-2 outline-green-500 outline-offset-2" />
+          </div>
+          <div
+            class="w-full h-40 bg-slate-800 rounded-lg absolute z-10 right-0 top-0 opacity-0 invisible group-hover:visible group-hover:opacity-100 transition-all">
+          </div>
+        </div>
+      </div>
     </div>
   </header>
-
-  <RouterView />
+  <div>
+  </div>
 </template>
--- src/components/HomeTitle.vue
+++ src/components/HomeTitle.vue
@@ -1,7 +1,44 @@
 <template>
-  <h1 class="group text-6xl font-bold tracking-wider font-mono text-blue-300 p-4">
-    <a href="mailto:paulmcglinchey789@gmail.com">
-      <span class="group">paulmcglinchey</span><span class="opacity-0 group group-hover:opacity-100 transition-all duration-300">789@gmail.com</span>
+  <h1 class="group text-6xl font-bold tracking-wider font-mono text-green-500 p-4">
+    <a href="mailto:paulmcglinchey789@gmail.com" class="relative before:w-1 before:max-h-full before:py-8 before:bg-green-500 before:block before:absolute before:bottom-0 animated-caret">
+      <span>paul mcglinchey</span>
     </a>
   </h1>
 </template>
+
+<style scoped>
+
+a.animated-caret::before {
+  animation: flicker 0.9s linear infinite reverse both;
+  animation: typing 2s linear both;
+}
+
+@keyframes flicker {
+  0% {
+    opacity: 0%;
+  }
+
+  20%, 100% {
+    opacity: 70%;
+  }
+}
+
+@keyframes typing {
+  0% {
+    left: 0%;
+  }
+
+  10% {
+    left: 4%;
+  }
+
+  20% {
+    left: 30%
+  }
+
+  100% {
+    left: 100%;
+  }
+}
+
+</style>
